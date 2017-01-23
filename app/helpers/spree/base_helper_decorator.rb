e92b8bafb5c29dbfa45b6d9abffb3c3777684750
shahzebjaved <shahzebjaved23@gmail.com>
2017-01-23 17:17:31 +0500

    changed the base_helper_decorater to accept only currency in price in

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,7 +1,8 @@
 module Spree
   BaseHelper.class_eval do
     def display_price(product_or_variant)
-      product_or_variant.price_in(current_currency, current_store.id, spree_current_user.try(:price_book_role_ids)).display_price.to_html
+      # product_or_variant.price_in(current_currency, current_store.id, spree_current_user.try(:price_book_role_ids)).display_price.to_html
+      product_or_variant.price_in(current_currency).display_price.to_html
     end
   end
 end
